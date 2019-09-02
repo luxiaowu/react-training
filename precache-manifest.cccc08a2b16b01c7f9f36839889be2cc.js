c2b95f83cbec4fd3a2668c502ce9234f715b693d
luxiaowu <luxiaowu@dachagui.com>
2019-09-02 17:19:36 +0800

    Updates

---
+++ precache-manifest.cccc08a2b16b01c7f9f36839889be2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a3e0687e049d2207f15ada2298b542",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "a2381f8223fdba45b00b",
+    "url": "/react-training/static/css/main.d5ce6785.chunk.css"
+  },
+  {
+    "revision": "d33542b7f71a5c7b0333",
+    "url": "/react-training/static/js/2.2a06e4c9.chunk.js"
+  },
+  {
+    "revision": "a2381f8223fdba45b00b",
+    "url": "/react-training/static/js/main.515dbfc4.chunk.js"
+  },
+  {
+    "revision": "0f0e249ecb6195896b88",
+    "url": "/react-training/static/js/runtime~main.6124b3f9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-training/static/media/logo.ee7cd8ed.svg"
+  }
+]);
